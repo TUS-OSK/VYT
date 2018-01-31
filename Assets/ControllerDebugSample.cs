c802c7fddc7a23e2ddaf2a831d4c1a551a0995eb
Moosan <washer2016@outlook.jp>
2018-01-31 15:51:06 +0900

    doyaro

---
+++ Assets/ControllerDebugSample.cs
@@ -0,0 +1,73 @@
+﻿using UnityEngine;
+using System.Collections;
+using Valve.VR.InteractionSystem;
+
+public class ControllerDebugSample : VRObjectBase
+{
+    private bool IsAttached;
+    void Update()
+    {
+        if (device != null)
+        {
+            if (device.GetTouchDown(SteamVR_Controller.ButtonMask.Trigger))
+            {
+                Debug.Log("トリガーを浅く引いた");
+            }
+            if (device.GetPressDown(SteamVR_Controller.ButtonMask.Trigger))
+            {
+                Debug.Log("トリガーを深く引いた");
+            }
+            if (device.GetTouchUp(SteamVR_Controller.ButtonMask.Trigger))
+            {
+                Debug.Log("トリガーを離した");
+            }
+            if (device.GetPressDown(SteamVR_Controller.ButtonMask.Touchpad))
+            {
+                Debug.Log("タッチパッドをクリックした");
+            }
+            if (device.GetPress(SteamVR_Controller.ButtonMask.Touchpad))
+            {
+                Debug.Log("タッチパッドをクリックしている");
+            }
+            if (device.GetPressUp(SteamVR_Controller.ButtonMask.Touchpad))
+            {
+                Debug.Log("タッチパッドをクリックして離した");
+            }
+            if (device.GetTouchDown(SteamVR_Controller.ButtonMask.Touchpad))
+            {
+                Debug.Log("タッチパッドに触った");
+            }
+            if (device.GetTouchUp(SteamVR_Controller.ButtonMask.Touchpad))
+            {
+                Debug.Log("タッチパッドを離した");
+            }
+            if (device.GetPressDown(SteamVR_Controller.ButtonMask.ApplicationMenu))
+            {
+                Debug.Log("メニューボタンをクリックした");
+            }
+            if (device.GetPressDown(SteamVR_Controller.ButtonMask.Grip))
+            {
+                Debug.Log("グリップボタンをクリックした");
+            }
+
+            if (device.GetTouch(SteamVR_Controller.ButtonMask.Trigger))
+            {
+                Debug.Log("トリガーを浅く引いている");
+                float value = device.GetAxis(Valve.VR.EVRButtonId.k_EButton_SteamVR_Trigger).x;
+                Debug.Log("トリガーが" + value * 100 + "%押されている");
+            }
+            if (device.GetPress(SteamVR_Controller.ButtonMask.Trigger))
+            {
+                Debug.Log("トリガーを深く引いている");
+                float value = device.GetAxis(Valve.VR.EVRButtonId.k_EButton_SteamVR_Trigger).x;
+                Debug.Log("トリガーが" + value * 100 + "%押されている");
+            }
+            if (device.GetTouch(SteamVR_Controller.ButtonMask.Touchpad))
+            {
+                Debug.Log("タッチパッドに触っている");
+                Vector2 position = device.GetAxis();
+                Debug.Log("x: " + position.x + " y: " + position.y + "らへんを触っている");
+            }
+        }
+    }
+}
---
+++ Assets/Scripts/IKController.cs
@@ -0,0 +1,79 @@
+﻿using UnityEngine;
+using System;
+using System.Collections;
+
+[RequireComponent(typeof(Animator))]
+
+public class IKController : MonoBehaviour
+{
+
+    protected Animator animator;
+    
+    public bool ikActive;
+    public Transform rightHandObj;
+    public Transform leftHandObj;
+    public Transform rightFootObj;
+    public Transform leftFootObj;
+    public Transform lookObj;
+
+    void Start()
+    {
+        animator = GetComponent<Animator>();
+    }
+    
+    void OnAnimatorIK()
+    {
+        if (animator)
+        {
+            if (ikActive)
+            {
+                if (lookObj != null)
+                {
+                    animator.SetLookAtWeight(1);
+                    animator.SetLookAtPosition(lookObj.position);
+                }
+                if (rightHandObj != null)
+                {
+                    animator.SetIKPositionWeight(AvatarIKGoal.RightHand, 1);
+                    animator.SetIKRotationWeight(AvatarIKGoal.RightHand, 1);
+                    animator.SetIKPosition(AvatarIKGoal.RightHand, rightHandObj.position);
+                    animator.SetIKRotation(AvatarIKGoal.RightHand, rightHandObj.rotation);
+                }
+                if (leftHandObj != null)
+                {
+                    animator.SetIKPositionWeight(AvatarIKGoal.LeftHand, 1);
+                    animator.SetIKRotationWeight(AvatarIKGoal.LeftHand, 1);
+                    animator.SetIKPosition(AvatarIKGoal.LeftHand, leftHandObj.position);
+                    animator.SetIKRotation(AvatarIKGoal.LeftHand, leftHandObj.rotation);
+                }
+                if (rightFootObj != null)
+                {
+                    animator.SetIKPositionWeight(AvatarIKGoal.RightFoot, 1);
+                    animator.SetIKRotationWeight(AvatarIKGoal.RightFoot, 1);
+                    animator.SetIKPosition(AvatarIKGoal.RightFoot, rightFootObj.position);
+                    animator.SetIKRotation(AvatarIKGoal.RightFoot, rightFootObj.rotation);
+                }
+                if (leftFootObj != null)
+                {
+                    animator.SetIKPositionWeight(AvatarIKGoal.LeftFoot, 1);
+                    animator.SetIKRotationWeight(AvatarIKGoal.LeftFoot, 1);
+                    animator.SetIKPosition(AvatarIKGoal.LeftFoot, leftFootObj.position);
+                    animator.SetIKRotation(AvatarIKGoal.LeftFoot, leftFootObj.rotation);
+                }
+            }
+            else
+            {
+                animator.SetIKPositionWeight(AvatarIKGoal.RightHand, 0);
+                animator.SetIKRotationWeight(AvatarIKGoal.RightHand, 0);
+                animator.SetIKPositionWeight(AvatarIKGoal.LeftHand, 0);
+                animator.SetIKRotationWeight(AvatarIKGoal.LeftHand, 0);
+
+                animator.SetIKPositionWeight(AvatarIKGoal.RightFoot, 0);
+                animator.SetIKRotationWeight(AvatarIKGoal.RightFoot, 0);
+                animator.SetIKPositionWeight(AvatarIKGoal.LeftFoot, 0);
+                animator.SetIKRotationWeight(AvatarIKGoal.LeftFoot, 0);
+                animator.SetLookAtWeight(0);
+            }
+        }
+    }
+}
