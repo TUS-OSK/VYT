baeb4e3e8c323576b61940f984129a9597d6f264
Moosan <washer2016@outlook.jp>
2018-01-31 20:07:37 +0900

    camera

---
+++ Assets/PlayerWalk.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerWalk : handEvent {
+    [SerializeField]
+    private float speed;
+    [SerializeField]
+    private GameObject pa;
+
+    void Update() {
+        if (device.GetPress(SteamVR_Controller.ButtonMask.Touchpad))
+        {
+            Vector2 position = device.GetAxis();
+            pa.transform.position = pa.transform.position + new Vector3(position.x, 0, position.y).normalized * speed;
+        }
+    }
+}
