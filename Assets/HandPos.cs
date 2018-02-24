5aa7f7e2e63d67873dad6105773adc470f5d6660
Moosan <washer2016@outlook.jp>
2018-02-24 20:33:59 +0900

    modelkesita

---
+++ Assets/HandPos.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HandPos : MonoBehaviour {
+    [SerializeField]
+    private GameObject target;
+	void Update () {
+        Vector3 pos = target.transform.position;
+        transform.position = new Vector3(pos.x,pos.y-2.81f,pos.z);
+	}
+}
