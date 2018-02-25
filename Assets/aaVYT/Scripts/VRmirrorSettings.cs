48c72971992dacd455bdcf06ed2654c461733f6e
zmirez <1416040@ed.tus.ac.jp>
2018-02-25 18:41:50 +0900

    display change

---
+++ Assets/aaVYT/Scripts/VRmirrorSettings.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class VRmirrorSettings : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+        UnityEngine.VR.VRSettings.showDeviceView = false;
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
