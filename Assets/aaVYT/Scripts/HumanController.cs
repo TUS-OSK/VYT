eefaa0df771d7eb665b8152c6eab6b3260f3fc93
OSK-Master <os_ken@ed.tus.ac.jp>
2018-03-25 20:16:52 +0900

    morcap

--- Assets/aaVYT/Scripts/HumanController.cs
+++ Assets/aaVYT/Scripts/HumanController.cs
@@ -23,6 +23,8 @@ public class HumanController : MonoBehaviour {
     private Vector3 LeftHandLocalRotation;
     [SerializeField]
     private bool Move;
+    [SerializeField]
+    private GameObject Player;
     private void Start()
     {
         MoveSet(Move);
@@ -40,16 +42,20 @@ public class HumanController : MonoBehaviour {
         HeadObj.transform.localRotation = Head.transform.localRotation;
     }
     private void SetRightHandTransform() {
-        RightHandObj.transform.localRotation = RightHand.transform.localRotation;
+        RightHandObj.transform.localRotation = tyousei(RightHand.transform.localEulerAngles);
     }
     private void SetLeftHandTransform() {
-        LeftHandObj.transform.localRotation = LeftHand.transform.localRotation;
+        LeftHandObj.transform.localRotation = tyousei(LeftHand.transform.localEulerAngles);
     }
     public void MoveSet(bool move) {
         Move = move;
+        //Player.transform.SetPositionAndRotation(transform.position,transform.rotation);
         HeadObj.transform.localRotation = Quaternion.Euler(HeadLocalRotation);
         RightHandObj.transform.localRotation = Quaternion.Euler(RightHandLocalRotation);
         LeftHandObj.transform.localRotation = Quaternion.Euler(LeftHandLocalRotation);
         Update();
+    }
+    private Quaternion tyousei(Vector3 r) {
+        return Quaternion.Euler(new Vector3(-r.x,r.y,r.z));
     }
 }
