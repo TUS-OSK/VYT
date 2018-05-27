87e501caba612eec9a9487648be84086a49669b3
Moosan <washer2016@outlook.jp>
2018-05-27 20:07:14 +0900

    painter

---
+++ Assets/painter.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class painter : HandEvent {
+    public TrailRenderer tra;
+    private void FixedUpdate()
+    {
+        tra.enabled = Device.GetPress(SteamVR_Controller.ButtonMask.Grip);
+    }
+}
